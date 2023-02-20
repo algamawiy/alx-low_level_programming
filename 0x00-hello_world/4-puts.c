de39e0650ea5c8a59572e4baf9599b2e78d9db71
algamawiy <aliyuassalafiygama@gmail.com>
2023-02-20 15:48:58 -0800

    Modified 4-puts.c file

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 
 /** 
- * main - This function display a text
+ * main - Entry point
  *
  * Return:  0
-*/
+ */
 int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
